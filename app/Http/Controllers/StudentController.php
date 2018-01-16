7e2d4f70ba5222e3856243a448d4444fe80b21b6
DESKTOP-9QJEEPU\paopao <ptxt4wrd@gmail.com>
2018-01-16 19:24:29 +0800

    Init Commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Student;
+use Carbon\Carbon;
+
+class StudentController extends Controller
+{
+    //
+
+    public function add_student(Request $request) {
+        // return $request;
+
+        $dt = Carbon::parse($request->birthday);
+
+        $s = new Student();
+        $s->facebook_id = "N/A";
+        $s->firstname = $request->firstname;
+        $s->lastname = $request->lastname;
+        $s->birthday = $dt;
+        $s->email = $request->email;
+        $s->mobile = $request->mobile;
+        $s->password = "123456";
+        $s->type = (int)$request->type;
+        $s->status = (int)$request->status;
+
+        if($s->save()) {
+            return array(
+                "Status" => 200,
+                "Message" => "Success"
+            );
+        }
+
+        return array(
+            "Status" => 500,
+            "Message" => "Error"
+        );
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get("/{name?}", function($name = null) {
+
+    if($name == null) {
+        return view('welcome');
+    }
+
+    return ["Name" => $name];
+});
+
+Route::get("/v1/add-student", "StudentController@add_student");
